33811ef94112d52c3cc3faf78a61fd42702eb06a
daffautomo23 <daffautomo54@gmail.com>
2026-03-25 15:54:25 +0700

    chore: regenerate Next.js build output files and cache.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "adec51bf5abbccd2"; }
+/******/ 		__webpack_require__.h = function() { return "815d531a98473c39"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
